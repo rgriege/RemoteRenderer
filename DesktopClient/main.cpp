9d44a23e74a16fda81230e1ab06350cf427f120d
Ryan Griege <rgriege@sbcglobal.net>
2014-03-01 10:54:25 -0600

    progress on desktop client

--- DesktopClient/main.cpp
+++ DesktopClient/main.cpp
@@ -3,10 +3,24 @@
 #include <sstream>
 #include <iostream>
 #include <GL/freeglut.h>
-#include <NetworkManager.h>
 #include <Decoder.h>
 #include <rgbData.h>
-#include <NetworkStream.h>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+#include <thread>
+
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::bind;
+
+void _onMessage(connection_hdl hdl, client::message_ptr msg)
+{
+    const void* data = msg->get_payload().c_str();
+    decoder->decode_frame(static_cast<const uint8_t*>(data), msg->get_payload().length, buffer);
+    flip_image_vertically(buffer, width, height);
+    glutPostRedisplay();
+}
 
 int width = 300;
 int height = 300;
@@ -14,39 +28,35 @@ int buffer_size;
 uint8_t* buffer;
 Decoder* decoder;
 
-void cb_display()
+void display()
 {
     glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, buffer);
     glutSwapBuffers();
 }
 
 #undef main
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    NeTwerk::NetworkManager* netMgr = NeTwerk::NetworkManager::getInstance();
-
-    std::cout << "Enter server hostname: ";
-    std::string server = netMgr->getLocalHostname();
-    //std::cin >> server;
-    std::cout << "Attempting to connect to '" << server << "'..." << std::endl;
-    uint32_t serverSocketId = netMgr->connect(server.c_str());
-    if (serverSocketId == BAD_SOCKET_ID)
-        exit(EXIT_FAILURE);
-    else
-        std::cout << "connected to server!" << std::endl;
+    client client;
+    client.init_asio();
+    client.clear_access_channels(websocketpp::log::alevel::frame_payload);
+    client.clear_access_channels(websocketpp::log::alevel::frame_header);
+    client.set_message_handler(bind(&_onMessage, &client, _1, _2));
+    websocketpp::lib::error_code ec;
+    client::connection_ptr con = client.get_connection("ws://localhost:9001", ec);
+    client.connect(con);
+    std::thread thread(std::bind(&client::run, &client));
 
     decoder = new Decoder();
     decoder->bootstrap(AV_CODEC_ID_MPEG1VIDEO, width, height);
 
-    NetworkStream input_stream(serverSocketId);
-
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE );
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
     glutInitWindowSize(width, height);
-    glutCreateWindow( "OpenGL Remote Rendering Client");
-    glutDisplayFunc( cb_display );
+    glutCreateWindow("OpenGL Remote Rendering Client");
+    glutDisplayFunc(display);
 
-    printf("%d\n",glutGetWindow());
+    printf("%d\n", glutGetWindow());
 
     width = glutGet(GLUT_WINDOW_WIDTH);
     height = glutGet(GLUT_WINDOW_HEIGHT);
@@ -54,20 +64,13 @@ int main( int argc, char **argv )
     buffer_size = width*height*3;
     buffer = (uint8_t*) malloc(buffer_size);
 
-    int bytes_read = 0;
-    for (;;)
-    {
-        netMgr->prime();
-        decoder->decode_frame(input_stream, buffer);
-        flip_image_vertically(buffer, width, height);
-        glutPostRedisplay();
-
+    for (;;) {
         glutMainLoopEvent();
     }
 
     printf("Video playback complete\n");
 
-    glutMainLoop( );
+    glutMainLoop();
 
     delete decoder;
     free(buffer);
